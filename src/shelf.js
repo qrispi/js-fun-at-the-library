55be295499b9dcb061889a8848dfbd04b8e7c08f
Christopher Baum <christopherabaum@gmail.com>
2022-11-30 23:56:18 -0600

    Complete searchShelf function

--- src/shelf.js
+++ src/shelf.js
@@ -1,27 +1,35 @@
 var sciFiShelf = []
 var extraBooks = []
+
 function shelfBook(book, shelf) {
   shelf.length >= 3 ? extraBooks.unshift(book) : shelf.unshift(book)
   return shelf
 }
 
 var checkedOut = []
+
 function unshelfBook(title, shelf) {
   var shelfPosition = shelf.findIndex((book) => book.title === title)
   return checkedOut = shelf.splice(shelfPosition, 1)
 }
 
 function listTitles(shelf) {
-var titles = []
-for (i=0; i<shelf.length; i++) {
-  titles.splice(i, 0, shelf[i].title)
-}
-return titles.join(', ')
+  var titles = []
+  for (i=0; i<shelf.length; i++) {
+    titles.splice(i, 0, shelf[i].title)
+  }
+  return titles.join(', ')
 }
 
-function searchShelf() {
-
+function searchShelf(shelf, title) {
+  for (i=0; i<shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true
+    }
+  }
+  return false
 }
+
 
 module.exports = {
   shelfBook,
