d51b3523e727249ac5e4ef51e7b18f0bfe39fd7a
Christopher Baum <christopherabaum@gmail.com>
2022-12-01 01:05:56 -0600

    Complete checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -15,7 +15,13 @@ function addBook(library, book) {
 }
 
 function checkoutBook(library, title, genre) {
-
+  var shelfPosition = library['shelves'][genre].findIndex((book) => book.title === title)
+  if (shelfPosition !== -1) {
+    library['shelves'][genre].splice(shelfPosition, 1)
+    return `You have now checked out ${title} from the ${library.name}`
+  } else {
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  }
 }
 
 
