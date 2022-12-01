41eb7c2d5aa90be74d683185ed5485c924a55b4d
Christopher Baum <christopherabaum@gmail.com>
2022-12-01 00:06:12 -0600

    Create function skeletons for library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,18 @@
+function createLibrary(name) {
+
+}
+
+function addBook(library, book) {
+
+}
+
+function checkoutBook(library, title, genre) {
+
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
