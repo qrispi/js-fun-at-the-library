eb1238cf243883e15c68922a2b8ea863ed37366f
Christopher Baum <christopherabaum@gmail.com>
2022-11-30 19:42:30 -0600

    Add all book functions and pass all tests

--- src/book.js
+++ src/book.js
@@ -1,18 +1,43 @@
 function createTitle(title) {
-  var madAdjs = ["Wacky", "Lop-Sided", "Mysterious", "Obese", "Flaccid", "Moist", "Extremely Smelly"]
-  var madNouns = ["Grandpa", "Tooth Fairy", "Gremlins", "Vampires", "House Guests", "Visitors from Outer Space"]
-  var madVerbs = ["Starving", "Raging", "Tootin'", "Puking", "Yelling", "Crying", "Growling", "Hovering"]
-  var a = Math.floor(Math.random()*madAdjs.length)
-  var n = Math.floor(Math.random()*madNouns.length)
-  var v = Math.floor(Math.random()*madVerbs.length)
-  return `${title.toUpperCase()} & the ${madAdjs[a]}, ${madVerbs[v]}, ${madNouns[n]}!`
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age ,pronouns) {
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+}
+
+var reviews = []
+
+function saveReview(string, array) {
+  return array.includes(string) ? array : array.push(string)
+}
+
+function calculatePageCount(title) {
+  return (title.length * 20)
+}
+
+function writeBook(title ,character ,genre) {
+  return book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  }
+}
+
+function editBook() {
+  return book.pageCount = (Math.ceil(book.pageCount * 0.75))
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
