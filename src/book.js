f4ac9a41e51f09a6ec19b6cd2cb3328a5222e120
Christopher Baum <christopherabaum@gmail.com>
2022-11-30 18:28:24 -0600

    Finish the createTitle function

--- src/book.js
+++ src/book.js
@@ -1,5 +1,11 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var madAdjs = ["Wacky", "Lop-Sided", "Mysterious", "Obese", "Flaccid", "Moist", "Extremely Smelly"]
+  var madNouns = ["Grandpa", "Tooth Fairy", "Gremlins", "Vampires", "House Guests", "Visitors from Outer Space"]
+  var madVerbs = ["Starving", "Raging", "Tootin'", "Puking", "Yelling", "Crying", "Growling", "Hovering"]
+  var a = Math.floor(Math.random()*madAdjs.length)
+  var n = Math.floor(Math.random()*madNouns.length)
+  var v = Math.floor(Math.random()*madVerbs.length)
+  return `${title.toUpperCase()} & the ${madAdjs[a]}, ${madVerbs[v]}, ${madNouns[n]}!`
 }
 
 module.exports = {
