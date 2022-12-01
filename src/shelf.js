56a663984785909cde1ccfb5b936b98f40c95460
Christopher Baum <christopherabaum@gmail.com>
2022-11-30 23:16:17 -0600

    Complete shelfBook and unshelfBook functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,9 +1,14 @@
-function shelfBook() {
-
+var sciFiShelf = []
+var extraBooks = []
+function shelfBook(book, shelf) {
+  shelf.length >= 3 ? extraBooks.unshift(book) : shelf.unshift(book)
+  return shelf
 }
 
-function unshelfBook() {
-
+var checkedOut = []
+function unshelfBook(title, shelf) {
+  var shelfPosition = shelf.findIndex((book) => book.title === title)
+  return checkedOut = shelf.splice(shelfPosition, 1)
 }
 
 function listTitles() {
