e73c2588ab1482b445a0b986e4a940d36bdaff05
Christopher Baum <christopherabaum@gmail.com>
2022-12-01 00:17:12 -0600

    Complete creatLibrary function

--- src/Library.js
+++ src/Library.js
@@ -1,9 +1,17 @@
 function createLibrary(name) {
-
+  return library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
 }
 
 function addBook(library, book) {
-
+  var properShelf = book.genre
+  return library.shelves.properShelf.push(book)
 }
 
 function checkoutBook(library, title, genre) {
