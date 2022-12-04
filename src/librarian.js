d75c50272794b42324a130f9fa9229eb8153ed6c
Christopher Baum <christopherabaum@gmail.com>
2022-12-04 13:59:12 -0600

    Complete findBook method

--- src/librarian.js
+++ src/librarian.js
@@ -9,15 +9,14 @@ class Librarian {
   findBook(bookTitle) {
     var shelfKeys = Object.keys(this.library.shelves)
     for (var i=0; i < shelfKeys.length; i++) {
-      var shelf = shelfKeys[i]
-      for (var x=0; x < shelf.length; x++) {
-        if (this.library.shelves[shelf][x].title === bookTitle) {
-          this.library.shelves[shelf].splice(x, 1)
+      for (var x=0; x < this.library.shelves[shelfKeys[i]].length; x++) {
+        if (this.library.shelves[shelfKeys[i]][x].title === bookTitle) {
+          this.library.shelves[shelfKeys[i]].splice(x, 1)
           return `Yes, we have ${bookTitle}`
         }
       }
-      return `Sorry, we do not have ${bookTitle}`
     }
+    return `Sorry, we do not have ${bookTitle}`
   }
   calculateLateFee(days) {
     return Math.ceil(days * 0.25)
