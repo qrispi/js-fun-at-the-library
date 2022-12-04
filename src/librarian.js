614a12e479f07896254f8d294de192f030c60c40
Christopher Baum <christopherabaum@gmail.com>
2022-12-04 12:54:27 -0600

    Add first draft of findBook

--- src/librarian.js
+++ src/librarian.js
@@ -6,6 +6,19 @@ class Librarian {
   greetPatron(name, morning) {
     return (morning) ? `Good morning, ${name}!` : `Hello, ${name}!`
   }
+  findBook(bookTitle) {
+    var shelfKeys = Object.keys(this.library.shelves)
+    for (var i=0; i < shelfKeys.length; i++) {
+      var shelf = shelfKeys[i]
+      for (var x=0; x < shelf.length; x++) {
+        if (this.library.shelves[shelf][x].title === bookTitle) {
+          this.library.shelves[shelf].splice(x, 1)
+          return `Yes, we have ${bookTitle}`
+        }
+      }
+      return `Sorry, we do not have ${bookTitle}`
+    }
+  }
 }
 
 module.exports = Librarian
