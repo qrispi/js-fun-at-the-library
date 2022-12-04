6f0210f4fdc677ab9da6426b99dcfc8c8a42d9b9
Christopher Baum <christopherabaum@gmail.com>
2022-12-04 11:55:40 -0600

    Refactor and clean code

--- src/shelf.js
+++ src/shelf.js
@@ -1,22 +1,16 @@
-var sciFiShelf = []
-var extraBooks = []
-
 function shelfBook(book, shelf) {
   shelf.length >= 3 ? extraBooks.unshift(book) : shelf.unshift(book)
-  return shelf
 }
-
-var checkedOut = []
+var extraBooks = []
 
 function unshelfBook(title, shelf) {
-  var shelfPosition = shelf.findIndex((book) => book.title === title)
-  return checkedOut = shelf.splice(shelfPosition, 1)
+  shelf.splice(shelf.findIndex((book) => book.title === title), 1)
 }
 
 function listTitles(shelf) {
   var titles = []
   for (i=0; i<shelf.length; i++) {
-    titles.splice(i, 0, shelf[i].title)
+    titles.push(shelf[i].title)
   }
   return titles.join(', ')
 }
